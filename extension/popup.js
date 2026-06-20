9921d09e122a853acbb5596f5e87fddc0cad2be9
J. Bhargava Gopal <jbhargavagopal@gmail.com>
2026-06-21 01:59:27 +0530

    minor mistake 1

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/recommend/";
+const API_URL = "https://datagambit.onrender.com/recommend/";
 
 const btn = document.getElementById("analyseBtn");
 const statusBox = document.getElementById("status");
